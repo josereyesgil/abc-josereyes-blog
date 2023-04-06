fcd018e90c3a00170ba8cb44f311095fbc7ba3d8
José Reyes Gil Rubio <josereyesgil@gmail.com>
2023-04-06 10:42:30 +0200

    Deploy Heroku

---
+++ manage_post/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from ckeditor.fields import RichTextField
+# Create your models here.
+
+#Categoría
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='Categories', blank=False, null=False)
+    slug = models.SlugField(unique=True, max_length=40)
+    featured = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+#Artículo
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    introduction = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    image = models.ImageField(upload_to='Articles', blank=False, null=False)
+    body = RichTextField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    #1 Categoría: Redes // Object (1) Redes
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+#Calificaciones
+class Rating(models.Model):
+    value = models.FloatField()
+    description = models.CharField(max_length=255)
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user_id.username
+
+    class Meta:
+        verbose_name = 'Rating'
+        verbose_name_plural = 'Ratings'
---
+++ user/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from decouple import config
+from django.core.mail import send_mail
+
+# Create your models here.
+
+class User(AbstractUser):
+    full_name = models.CharField(max_length=255)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    photo = models.ImageField(default='Profiles/default-user.png',upload_to='Profiles', blank=True, null=True)
+    profession = models.CharField(max_length=50, null=True)
+    about = models.TextField(null=True)
+    birthday = models.DateField(null=True)
+    twitter = models.URLField(max_length=50, null=True)
+    linkedin = models.URLField(max_length=50, null=True)
+    facebook = models.URLField(max_length=50, null=True)
+    tiktok = models.URLField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def email(sender, instance, created, **kwargs):
+    if created:
+        send_mail(
+            'Bienvenido a nuestro blog',
+            str('Hola ' + instance.full_name + ', usted se ha registrado satisfactoriamente en el blog.'
+                                              '¡Es un placer que seas parte de nuestra familia!'),
+            config('EMAIL_HOST_USER'),
+            [instance.email]
+        )
